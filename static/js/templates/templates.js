1b8be052321b8d18262dc04998ca40acf54175eb
Derek Wheelden <derek.wheelden@gmail.com>
2016-01-16 22:11:53 -0500

    Random foods are a thing :tada:

--- static/js/templates/templates.js
+++ static/js/templates/templates.js
@@ -5,16 +5,15 @@ this["templates"]["geocoding"] = Handlebars.template({"compiler":[7,">= 4.0.0"],
 this["templates"]["gettingVenues"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<div class=\"loading venues\">\n    Getting hungry?\n</div>\n";
 },"useData":true});
-this["templates"]["which"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
-    var helper;
+this["templates"]["nope"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"error nope\">\n    <img src=\"https://media.giphy.com/media/10oRQhnkcc72Le/giphy.gif\">\n</div>\n";
+},"useData":true});
+this["templates"]["which"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "        "
-    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
-    + "\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1;
-
-  return "<div class=\"which\">\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.venues : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</div>\n";
+  return "<div class=\"which\">\n    "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.which : depth0)) != null ? stack1.name : stack1), depth0))
+    + " from "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.venue : depth0)) != null ? stack1.name : stack1), depth0))
+    + "\n</div>\n";
 },"useData":true});
--- static/js/which.js
+++ static/js/which.js
@@ -12,26 +12,50 @@ Which.prototype.home = function () {
 
     var _this = this;
 
+    if ("geolocation" in navigator) {
+        this.geoLocate();
+    }
+
+    if (window.location.hash) {
+        this.searchVenues(window.location.hash.substring(1).split(','));
+    }
+
     $('form').submit(function () {
 
         _this.replaceTemplate(templates.geocoding);
 
-        _this.geoLocate($(this).serializeArray(), function (lat, lng) {
+        _this.geocode($(this).serializeArray(), function (lat, lng) {
 
-            _this.replaceTemplate(templates.gettingVenues);
-
-            $.get('/which', { latitude : lat, longitude : lng }, function (data) {
-                var parsed = _.compact(_.map(JSON.parse(data).venues, function (venue) {
-                    if ('menus' in venue) return venue;
-                }));
-
-                _this.getWhich(parsed);
-            });
+            _this.searchVenues([lat, lng]);
 
         });
 
         return false;
 
+    });
+
+};
+
+Which.prototype.searchVenues = function (latlng) {
+
+    var _this = this;
+
+    if (!latlng) {
+        this.replaceTemplate(templates.nope);
+        return false;
+    }
+
+    this.replaceTemplate(templates.gettingVenues);
+
+    window.location.hash = latlng[0] + ',' + latlng[1];
+
+    $.get('/which', { latitude : latlng[0], longitude : latlng[1] }, function (data) {
+
+        var parsed = _.compact(_.map(JSON.parse(data).venues, function (venue) {
+            if ('menus' in venue) return venue;
+        }));
+
+        _this.getWhich(parsed);
     });
 
 };
@@ -46,21 +70,52 @@ Which.prototype.replaceTemplate = function (template, context) {
 
 };
 
-Which.prototype.geoLocate = function (data, cb) {
+Which.prototype.geoLocate = function () {
+
+    var $form = $('.search-form');
+    var geocoder = new google.maps.Geocoder();
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        var options = {
+            location : {
+                lat : position.coords.latitude,
+                lng : position.coords.longitude
+            }
+        };
+
+        geocoder.geocode(options, function (results, status) {
+
+            var address = results[0].address_components;
+
+            if ($form.find('#address').val()) return;
+
+            $form.find('#address').val(address[0].long_name + ' ' + address[1].long_name);
+            $form.find('#city').val(address[2].long_name);
+            $form.find('#state').val(address[6].short_name);
+
+        });
+
+    });
+
+};
+
+Which.prototype.geocode = function (data, cb) {
 
     var geocoder = new google.maps.Geocoder();
 
     var options = {
-        address : getAddressPart(data, 'address') +
-            getAddressPart(data, 'city') +
-            getAddressPart(data, 'state') +
-            getAddressPart(data, 'zip')
+        address : getAddressPart(data, 'address') + ' ' +
+            getAddressPart(data, 'city') + ' ' +
+            getAddressPart(data, 'state')
     };
 
     geocoder.geocode(options, function (results, status) {
 
         if (results && results.length) {
             cb(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        } else {
+            cb();
         }
 
     });
@@ -75,7 +130,44 @@ Which.prototype.geoLocate = function (data, cb) {
 
 Which.prototype.getWhich = function (parsed) {
 
-    this.replaceTemplate(templates.which, { venues : parsed });
+    if (!parsed || !parsed.length) {
+        this.replaceTemplate(templates.nope);
+        return;
+    }
+
+    try {
+        var context = getRandomMenuItem();
+        this.replaceTemplate(templates.which, context);
+    } catch (err) {
+        this.replaceTemplate(templates.nope);
+    }
+
+    function getRandomMenuItem (attempt) {
+
+        if (attempt && attempt > 10) {
+            throw new Error('Could\'t find anything to eat');
+        }
+
+        var venueIndex = Math.floor(Math.random() * (parsed.length - 1));
+        var menuIndex = Math.floor(Math.random() * (parsed[venueIndex].menus.length - 1));
+        var sectionIndex = Math.floor(Math.random() * (parsed[venueIndex].menus[menuIndex].sections.length - 1));
+        var subSectionIndex = Math.floor(Math.random() * (parsed[venueIndex].menus[menuIndex].sections[sectionIndex].subsections.length - 1));
+        var contents = _.compact(_.map(parsed[venueIndex].menus[menuIndex].sections[sectionIndex].subsections[subSectionIndex].contents), function (content) {
+            if ('name' in content) return content;
+        });
+        var contentsIndex = Math.floor(Math.random() * (contents.length - 1));
+
+        attempt = attempt || 1;
+
+        if (!contents[contentsIndex].name) {
+            return getRandomMenuItem(attempt + 1);
+        }
+
+        return {
+            venue: parsed[venueIndex],
+            which: contents[contentsIndex]
+        };
+    }
 
 };
 
